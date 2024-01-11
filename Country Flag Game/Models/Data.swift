0e1e7ed6e0e53768d31e2f163043fffd1a2c75bd
Phillip Mantatsky <pmantatsky1@bsd220.org>
2024-01-11 09:48:34 -0600

    Created Data and Quiz Manager

---
+++ Country Flag Game/Models/Data.swift
@@ -0,0 +1,36 @@
+//
+//  Data.swift
+//  Country Flag Game
+//
+//  Created by Phillip Mantatsky on 1/11/24.
+//
+
+import Foundation
+struct Data {
+
+    let questions = [
+        Question(correctAnswer:
+                Answer(text: "England", isCorrect: true),
+                 incorrectAnswer: [
+                Answer(text: "Ireland", isCorrect: false),
+                Answer(text: "Iceland", isCorrect: false),
+                Answer(text: "South Africa", isCorrect: false)
+              ])
+        ,
+        Question(correctAnswer:
+                Answer(text: "France", isCorrect: true),
+                 incorrectAnswer: [
+                Answer(text: "Spain", isCorrect: false),
+                Answer(text: "Italy", isCorrect: false),
+                Answer(text: "Germany", isCorrect: false)
+           ])
+        ,
+        Question(correctAnswer:
+                Answer(text: "Germany", isCorrect: true),
+                 incorrectAnswer: [
+                Answer(text: "Austria", isCorrect: true),
+                Answer(text: "Italy", isCorrect: false),
+                Answer(text: "Czech Republic", isCorrect: false)
+           ])
+    ]
+}
